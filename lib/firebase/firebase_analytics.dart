8302575f92a232ee5a5398c99d4115fd47672d9a
gprinc <gprinc@itba.edu.ar>
2023-06-17 17:31:02 -0300

    added firebase auth method

--- lib/firebase/firebase_analytics.dart
+++ lib/firebase/firebase_analytics.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 
-class Analytics {
+class FirebaseAnalyticsITBA {
   static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
   static FirebaseAnalyticsObserver observer =
       FirebaseAnalyticsObserver(analytics: analytics);
 
   void onLogin() {         
-          analytics.logLogin();
-         // similar to: analytics.logEvent(name: 'login');
-     }
+      analytics.logLogin();
+  }
    
    void onEvent(String event, Map<String, Object> params) {
        analytics.logEvent(
---
+++ lib/firebase/firebase_authentication.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthenticationITBA {
+
+  final _auth = FirebaseAuth.instance;
+  late User? firebaseUser;
+  
+  @override
+  void onReady() {
+    firebaseUser = _auth.currentUser;
+  }
+
+  Future<UserCredential?> registerUser(String email, String password) async {
+    try {
+      return await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        loginUser(email, password);
+      }
+      return null;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<UserCredential?> loginUser(String email, String password) async {
+    try {
+      return await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+      return null;
+    }
+  }
+
+  void logout() async {
+    await _auth.signOut();
+  }
+}
