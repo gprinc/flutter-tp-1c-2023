176b85bb49925cd4a0e0d31e7051b733f05a56ea
agbossi <abossi@itba.edu.ar>
2023-06-22 14:42:45 -0300

    clase duplicada

--- lib/molecules/buttons.dart
+++ lib/molecules/buttons.dart
@@ -178,35 +178,3 @@ class _IconSplashButtonState extends State<IconSplashButton> {
     );
   }
 }
-
-class IconSplashButton extends StatefulWidget {
-  const IconSplashButton(
-      {super.key,
-      required this.icon,
-      required this.onPress,
-      this.enabledState = true});
-
-  final IconData icon;
-  final void Function() onPress;
-  final bool enabledState;
-
-  @override
-  State<IconSplashButton> createState() => _IconSplashButtonState();
-}
-
-class _IconSplashButtonState extends State<IconSplashButton> {
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 14,
-      width: 14,
-      child: InkWell(
-          borderRadius: BorderRadius.circular(100),
-          onTap: widget.enabledState ? () => widget.onPress() : null,
-          child: Icon(
-            widget.icon,
-            color: btnSecondary,
-          )),
-    );
-  }
-}
